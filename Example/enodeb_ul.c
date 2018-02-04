445e30080c305367ec7b3d7a5108376aca7920b4
Rimon Orni <rimon.orni@orimon.tandemg.local>
2018-02-04 15:22:04 +0200

    MagicBox: LTE: Calculate the EVM itself

--- Example/enodeb_ul.c
+++ Example/enodeb_ul.c
@@ -46,19 +46,79 @@ void SetConfiguration(
 	hoppingConfig->hopping_offset = 4;
 }
 
-int DecodeDataChannel(srslte_enb_ul_t* uplinkConfig)
+void PrepareToDecode(srslte_enb_ul_t *uplinkConfig, cf_t* signal)
+{
+	srslte_cell_t cell;
+	srslte_prach_cfg_t prach_cfg;
+	srslte_refsignal_dmrs_pusch_cfg_t puschConfig;
+	srslte_pusch_hopping_cfg_t hoppingConfig;
+	srslte_pucch_cfg_t pucchConfig;
+
+	SetConfiguration(&cell, &prach_cfg, &puschConfig, &hoppingConfig, &pucchConfig);
+
+	if (srslte_enb_ul_init(uplinkConfig, cell, &prach_cfg, &puschConfig, &hoppingConfig, &pucchConfig, 1))
+	{
+		printf("Failed to init uplink\n");
+		return;
+	}
+
+	unsigned int resultSize = uplinkConfig->pusch.max_re;
+
+	uplinkConfig->pusch.q = srslte_vec_malloc(sizeof(int16_t) * resultSize * srslte_mod_bits_x_symbol(SRSLTE_MOD_QPSK));
+	if (!uplinkConfig->pusch.q) {
+		printf("Failed to allocate LLRs\n");
+		return;
+	}
+
+	uplinkConfig->pusch.d = srslte_vec_malloc(sizeof(cf_t) * resultSize);
+	if (!uplinkConfig->pusch.d) {
+		printf("Failed to allocate distances\n");
+		return;
+	}
+
+	uplinkConfig->pusch.ce = srslte_vec_malloc(sizeof(cf_t) * resultSize);
+	if (!uplinkConfig->pusch.ce) {
+		printf("Failed to allocate CEs\n");
+		return;
+	}
+
+	srslte_pucch_set_threshold(&(uplinkConfig->pucch), 1, 0);
+
+	srslte_enb_ul_cfg_rnti(uplinkConfig, 0, 1);
+	
+	// Perform fft on the signal
+	srslte_enb_ul_fft(uplinkConfig, signal);
+}
+
+void CalculateEVM(cf_t* distances, double* EVMs, unsigned int resultSize)
+{
+	cf_t refSignalPos = sqrt(2) + 1 * I;
+	cf_t refSignalNeg = -1 * sqrt(2) + 1 * I;
+
+	for (unsigned int i = 0; i < resultSize; i++)
+	{
+		cf_t refSignal = (__real__ distances[i] > 0) ? refSignalPos : refSignalNeg;
+
+		EVMs[i] = 100 * cabsf(distances[i] - refSignal);
+	}
+}
+
+
+void DecodeDataChannel(cf_t* signal, const unsigned int signalLen, unsigned int* resultSize, cf_t** distances, cf_t** CEs, short** LLRs)
 {
 	srslte_ra_ul_dci_t      grant;
 	srslte_dci_location_t   location;
 	srslte_softbuffer_rx_t softbuffer;
+	srslte_enb_ul_t uplinkConfig;
+
+	PrepareToDecode(&uplinkConfig, signal);
+	*resultSize = uplinkConfig.pusch.max_re;
 
 	// Copy grant info
 	memset(&grant, 0, sizeof(srslte_ra_dl_dci_t));
 	memset(&location, 0, sizeof(srslte_dci_location_t));
 	grant.mcs_idx = 6;
-	srslte_softbuffer_rx_init(&softbuffer, uplinkConfig->cell.nof_prb);
-
-	int res = -1;
+	srslte_softbuffer_rx_init(&softbuffer, uplinkConfig.cell.nof_prb);
 
 	srslte_ra_ul_dci_t dci; // Downlink Control Information
 	dci.freq_hop_fl = -1;
@@ -68,17 +128,17 @@ int DecodeDataChannel(srslte_enb_ul_t* uplinkConfig)
 
 	srslte_ra_ul_grant_t ulGrant;
 
-	if (srslte_ra_ul_dci_to_grant(&dci, uplinkConfig->cell.nof_prb, 0, &ulGrant, 0) != SRSLTE_SUCCESS)
+	if (srslte_ra_ul_dci_to_grant(&dci, uplinkConfig.cell.nof_prb, 0, &ulGrant, 0) != SRSLTE_SUCCESS)
 	{
 		printf("Error calculating the pusch grant\n");
-		return SRSLTE_ERROR;
+		return;
 	}
 
 	srslte_uci_data_t uci_data;
 	memset(&uci_data, 0, sizeof(srslte_uci_data_t));
 	uint8_t *data = malloc((1024 / 8) * 150);
 
-	srslte_enb_ul_get_pusch(uplinkConfig,
+	srslte_enb_ul_get_pusch(&uplinkConfig,
 		&ulGrant,
 		&softbuffer,
 		0,           // rnti_idx
@@ -89,61 +149,12 @@ int DecodeDataChannel(srslte_enb_ul_t* uplinkConfig)
 		0            // tti
 	);
 
-	fprintf(stderr, "DecodeDataChannel returning: %d\n", res);
+	*LLRs = uplinkConfig.pusch.q;
+	*distances = uplinkConfig.pusch.d;
+	*CEs = uplinkConfig.pusch.ce;
 
-	return res;
+	return;
 }
-
-void CalculateEVM(cf_t* signal, const unsigned int signalLen, unsigned int* resultSize, cf_t** distances, cf_t** CEs, short** LLRs)
-{
-	srslte_cell_t cell;
-	srslte_enb_ul_t uplinkConfig;
-	srslte_prach_cfg_t prach_cfg;
-	srslte_refsignal_dmrs_pusch_cfg_t puschConfig;
-	srslte_pusch_hopping_cfg_t hoppingConfig;
-	srslte_pucch_cfg_t pucchConfig;
-
-	SetConfiguration(&cell, &prach_cfg, &puschConfig, &hoppingConfig, &pucchConfig);
-
-	if (srslte_enb_ul_init(&uplinkConfig, cell, &prach_cfg, &puschConfig, &hoppingConfig, &pucchConfig, 1))
-	{
-		printf("Failed to init uplink\n");
-		return;
-	}
-
-	*resultSize = uplinkConfig.pusch.max_re;
-	*LLRs = srslte_vec_malloc(sizeof(int16_t) * *resultSize * srslte_mod_bits_x_symbol(SRSLTE_MOD_QPSK));
-	if (!LLRs) {
-		printf("Failed to allocate q\n");
-		return;
-	}
-
-	*distances = srslte_vec_malloc(sizeof(cf_t) * *resultSize);
-	if (!distances) {
-		printf("Failed to allocate d\n");
-		return;
-	}
-
-	*CEs = srslte_vec_malloc(sizeof(cf_t) * *resultSize);
-	if (!CEs) {
-		printf("Failed to allocate d\n");
-		return;
-	}
-
-	uplinkConfig.pusch.q = *LLRs;
-	uplinkConfig.pusch.d = *distances;
-	uplinkConfig.pusch.ce = *CEs;
-
-	srslte_pucch_set_threshold(&uplinkConfig.pucch, 1, 0);
-
-	srslte_enb_ul_cfg_rnti(&uplinkConfig, 0, 1);
-	
-	// Perform fft on the signal
-	srslte_enb_ul_fft(&uplinkConfig, signal);
-
-	DecodeDataChannel(&uplinkConfig);	
-}
-
 
 int readSignal(cf_t* sig, unsigned int maxSymbolsNum, const char *filename)
 {
@@ -169,13 +180,22 @@ int main(int argc, char** argv)
 	readSignal(pBuff, maxSigLen, filename);
 
 	cf_t* distances;
+	double* EVMs;
 	cf_t* CEs;
 	short* LLRs;
 	unsigned int resultSize;
 
-	CalculateEVM(pBuff, maxSigLen, &resultSize, &distances, &CEs, &LLRs);
 
+	DecodeDataChannel(pBuff, maxSigLen, &resultSize, &distances, &CEs, &LLRs);
 	printf("ResultSize: %d\n", resultSize);
+	
+	EVMs = srslte_vec_malloc(sizeof(double) * resultSize);
+	if (!EVMs) {
+		printf("Failed to allocate EVMs\n");
+		exit(1);
+	}
+	CalculateEVM(distances, EVMs, resultSize);
+
 	for (int i = 0; i < 50; i++)
 	{
 		printf("LLRs[%d] = %d\n", i, LLRs[i]);
@@ -183,7 +203,7 @@ int main(int argc, char** argv)
 	printf("\n\n");
 	for (int i = 0; i < 50; i++)
 	{
-		printf("distances[%d] = %f + %f I\n", i, __real__ distances[i], __imag__ distances[i]);
+		printf("EVMs[%d] = %f\n", i, EVMs[i]);
 	}
 	printf("\n\n");
 	for (int i = 0; i < resultSize - 3; i += 600)
@@ -194,14 +214,14 @@ int main(int argc, char** argv)
 		printf("\n");
 	}
 
-	char *distFilename = "C:\\temp\\LTE_Distances.txt";
+	char *EVMFilename = "C:\\temp\\LTE_EVMs.txt";
 	char *ceFilename = "C:\\temp\\LTE_CEs.txt";
-	FILE* distFile = fopen(distFilename, "w");
+	FILE* EVMFile = fopen(EVMFilename, "w");
 	FILE* ceFile = fopen(ceFilename, "w");
 
 	for (int i = 0; i < resultSize; i++)
 	{
-		fprintf(distFile, "%f + %f I\n", __real__ distances[i], __imag__ distances[i]);
+		fprintf(EVMFile, "%f\n", EVMs[i]);
 		fprintf(ceFile, "%f + %f I\n", __real__ CEs[i], __imag__ CEs[i]);
 	}
 }
